65384c9e3b38e34aa33b083f2debd7be7031c16f
leungs <116824832@qq.com>
2016-08-04 00:51:43 +0800

    home controller

---
+++ Taurus.Controllers/HomeViewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Taurus.Core;
+
+namespace Taurus.Controllers
+{
+    public class HomeViewController : ViewController
+    {
+        public void Index()
+        {
+        }
+        public void About() { }
+        public void Contact() { }
+    }
+}
