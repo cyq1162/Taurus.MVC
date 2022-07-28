2400cf69f4a3a2ed63502c5d875ead31bc9bb6e6
Aster <272657997@qq.com>
2022-07-28 16:26:04 +0800

    全局控制器，支持微服务。

--- Taurus.Controllers/DefaultController.cs
+++ Taurus.Controllers/DefaultController.cs
@@ -7,21 +7,27 @@ using System.Web;
 namespace Taurus.Controllers
 {
     /// <summary>
-    /// 默认控制器（此类不要动，partial类，可以在AriesController文件夹外建立自己的业务类）
+    /// 全局控制器（适全全局事件处理）
     /// </summary>
     public partial class DefaultController : Controller
     {
-        [HttpGet]
+        /// <summary>
+        /// 所有寻址不到的请求都集中执行到此方法。
+        /// </summary>
         public override void Default()
         {
             if (RouteConfig.RouteMode == 2 && Module == "api")
             {
                 Context.RewritePath("/text" + Context.Request.RawUrl);
             }
-            // Write("Hello world");
+            else
+            {
+                Write("DefaultController : Hello world");
+            }
         }
         /// <summary>
-        /// 用于登陆前的请求合法性验证，配合[Ack]属性
+        /// 用于所有的请求合法性验证，配合[Ack]属性
+        /// 启用时：局部的先执行（若存在)，无局部才执行全局。
         /// </summary>
         public static bool CheckAck(IController controller, string methodName)
         {
@@ -32,14 +38,26 @@ namespace Taurus.Controllers
 
         /// <summary>
         /// 用于需要登陆后的身份验证，配合[Token]属性
+        /// 启用时：局部的先执行（若存在)，无局部才执行全局。
         /// </summary>
         public static bool CheckToken(IController controller, string methodName)
         {
             //需要自己实现，或者通过配置Taurus.Auth启动自带的验证（自带的注释掉此方法即可）。
             return controller.CheckFormat("token Can't be Empty", "token");
         }
+
+        /// <summary>
+        /// 用于校验微服务的内部身份验证，配合[MicroService]属性
+        /// 启用时：全局仅此一个生效，局部的失效。
+        /// </summary>
+        public static bool CheckMicroService(IController controller, string methodName)
+        {
+            return MicroService.Config.ServerKey == controller.Request.Headers[MicroService.Const.HeaderKey];
+        }
+
         /// <summary>
         /// 全局【路由映射】
+        /// 启用时：所有请求都进入此地做映射。
         /// </summary>
         public static string RouteMapInvoke(HttpRequest request)
         {
@@ -51,6 +69,7 @@ namespace Taurus.Controllers
         }
         /// <summary>
         /// 全局【方法执行前拦截】
+        /// 启用时：先全局，再执行局部（若存在）。
         /// </summary>
         public static bool BeforeInvoke(IController controller, string methodName)
         {
@@ -73,6 +92,7 @@ namespace Taurus.Controllers
         }
         /// <summary>
         /// 全局【方法执行后业务】
+        /// 启用时：先执行局部（若存在），再执行全局。
         /// </summary>
         public static void EndInvoke(IController controller, string methodName)
         {
