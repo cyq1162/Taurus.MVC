3ebf19fc5b3f44bd351d163bd499fb0816b5b1e6
Aster <272657997@qq.com>
2022-09-14 04:18:54 +0800

    .

---
+++ demo/default/Taurus.View_NetCore/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+namespace Taurus.View
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+           // Taurus
+            services.AddDistributedMemoryCache();//支持Session的必要组件
+            services.AddSession();
+            services.AddHttpContext();
+            services.Configure<KestrelServerOptions>((x) =>
+            {
+                //x.Listen(IPAddress.Any, 443, op => {
+                //    op.UseHttps("xxx.pfx", "password");
+                //});
+
+                x.AllowSynchronousIO = true;
+                //为整个应用设置并发打开的最大 TCP 连接数,默认情况下，最大连接数不受限制 (NULL)
+                // x.Limits.MaxConcurrentConnections = 100000;
+                //对于已从 HTTP 或 HTTPS 升级到另一个协议（例如，Websocket 请求）的连接，有一个单独的限制。 连接升级后，不会计入 MaxConcurrentConnections 限制
+                // x.Limits.MaxConcurrentUpgradedConnections = 100000;
+                //获取或设置保持活动状态超时。 默认值为 2 分钟。
+                // x.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(100);
+                //  serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(1);
+            });//.Configure<IISServerOptions>(x => x.AllowSynchronousIO = true);
+            services.Configure<IISServerOptions>((x) =>
+            {
+                x.AllowSynchronousIO = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)//把IHostingEnvironment IWebHostEnvironment
+        {
+            app.UseWebSockets();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseHttpContext();
+            app.UseTaurusMvc(env);
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
